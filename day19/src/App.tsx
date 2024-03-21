ee913ab435bc88b6fbeaf866534f35560fe034bb
samade747 <samad.e747@gmail.com>
2024-03-21 17:41:36 +0500

    541pm

--- day19/src/App.tsx
+++ day19/src/App.tsx
@@ -1,36 +1,35 @@
+// @ts-ignore
 import React, { useState } from 'react';
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [numbers, setNumbers] = useState<number>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  const [numbers] = useState<number[]>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   const [doubledNumbers, setDoubledNumbers] = useState<number[]>([]);
 
   const doubleNumbers = () => {
-    const doubled = numbers.map((number: number) => number * 2);
-    setDoubledNumbers(doubled)
+    const doubled = numbers.map((num: number) => num * 2);
+    setDoubledNumbers(doubled);
   };
 
   return (
     <div>
-    <h2>Original Number</h2>
-    <ul>
-      {number.map((number: number) => (
-        <li key={number}>{number}</li>
-      ))}
-    </ul>
+      <h2>Original Numbers</h2>
+      <ul>
+        {numbers.map((num: number) => (
+          <li key={num}>{num}</li>
+        ))}
+      </ul>
 
-        <h2>Display double number</h2>
-        <ul>
-          {doubleNumbers.map((number: number) => (
-            <li key={number}>{number}</li>
-          ))}
-        </ul>
+      <h2>Doubled Numbers</h2>
+      <ul>
+        {doubledNumbers.map((num: number) => (
+          <li key={num}>{num}</li>
+        ))}
+      </ul>
 
-            <button onClick={doubleNumbers}>Doubel Numbers</button>
-  </div>
-  )
+      <button onClick={doubleNumbers}>Double Numbers</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
